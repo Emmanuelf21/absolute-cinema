ddc3ee1726715251173dad7beb355298448a226b
Emmanuel Franco <emmanuelfranco2001@gmail.com>
2025-06-12 16:53:24 -0300

    dropdown menu

--- src/components/BtnLogin.jsx
+++ src/components/BtnLogin.jsx
@@ -1,32 +1,64 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { FaUserCircle } from "react-icons/fa";
+import './css/btnlogin.css'
 
 const BtnLogin = () => {
+  const [usuarioLogado, setUsuarioLogado] = useState(null);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const navigate = useNavigate();
 
-    const [usuarioLogado, setUsuarioLogado] = useState(null);
-
-    useEffect(() => {
-        const usuario = Cookies.get('usuario_logado');
-        if (usuario) {
-            setUsuarioLogado(JSON.parse(usuario));
-        }
-    }, []);
-
-    if (usuarioLogado) {
-        return (
-            <NavLink to='/ingressos' className='btn-logado'>
-                <FaUserCircle />
-            </NavLink>
-        );
-    } else {
-        return (
-            <NavLink to='/login' className='btn-logar'>
-                Login/Cadastrar
-            </NavLink>
-        );
+  useEffect(() => {
+    const usuario = Cookies.get('usuario_logado');
+    if (usuario) {
+      setUsuarioLogado(JSON.parse(usuario));
     }
+  }, []);
+
+  const handleLogout = () => {
+    Cookies.remove('usuario_logado');
+    setUsuarioLogado(null);
+    setDropdownOpen(false);
+    navigate('/login');
+  };
+
+  if (usuarioLogado) {
+    return (
+      <div
+        className="btn-logado"
+        onMouseEnter={() => setDropdownOpen(true)}
+        onMouseLeave={() => setDropdownOpen(false)}
+      >
+        <FaUserCircle size={24} />
+        {dropdownOpen && (
+          <div
+            className="dropdown-menu"
+          >
+            <NavLink
+              to="/ingressos"
+              className="btn-ingressos"
+              onClick={() => setDropdownOpen(false)}
+            >
+              Ingressos
+            </NavLink>
+            <button
+              onClick={handleLogout} className='btn-sair'
+              
+            >
+              Sair
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  } else {
+    return (
+      <NavLink to='/login' className='btn-logar'>
+        Login/Cadastrar
+      </NavLink>
+    );
+  }
 }
 
-export default BtnLogin
+export default BtnLogin;
